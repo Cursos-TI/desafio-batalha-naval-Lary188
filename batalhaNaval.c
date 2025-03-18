0d692313d7fea005a52228ba7da6d39805fe4285
Lary188 <larydudadasilva6@gmail.com>
2025-03-18 19:42:42 +0000

    Pocisionamento dos Navios. Nivel Básico.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,80 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+   #define LINHAS 10
+   #define COLUNAS 10
+   #define NAVIO 3
+    int tabuleiro[LINHAS][COLUNAS];
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+    int naviohoriz[NAVIO] = {3, 3, 3};
+    int naviovert[NAVIO] = {3, 3, 3};
+
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = 0; // 0 para posição vazia(água)
+        }
+    }
+
+    int linhahoriz = 2;
+    int colunahoriz = 4;
+    int linhavert = 5;
+    int colunavert = 1;
+
+    if (colunahoriz + NAVIO <= COLUNAS) {
+    int podeposicionar = 1;
+
+    for (int i = 0; i < NAVIO; i++) {
+        if (tabuleiro[linhahoriz][colunahoriz + i] != 0) {
+            podeposicionar = 0;
+            break;
+        }
+    }
+
+        if (podeposicionar) {
+            for (int i = 0; i < NAVIO; i++) {
+                tabuleiro[linhahoriz][colunahoriz + i] = naviohoriz[i]; // 1 para posição ocupada(navio)
+            }
+        } else {
+            printf("Não é possível posicionar o navio sobre outro Navio.\n");
+            }
+        } else {
+            printf("Não é possível posicionar o navio fora do tabuleiro.\n");
+        }
+
+    if (linhavert + NAVIO <= LINHAS) {
+    int podeposicionar = 1; 
+
+    for (int i = 0; i < NAVIO; i++) {
+        if (tabuleiro[linhavert][colunavert + i] != 0) {
+            podeposicionar = 0;
+            break;
+        }
+    }
+
+        if (podeposicionar) {
+            for (int i = 0; i < NAVIO; i++) {
+                tabuleiro[linhavert][colunavert + i] = naviovert[i]; // 1 para posição ocupada(navio)
+            }
+        } else {
+            printf("Não é possível posicionar o navio sobre outro Navio.\n");
+            }
+        } else {
+            printf("Não é possível posicionar o navio fora do tabuleiro.\n");
+        }
+
+        for (int i = 0; i < LINHAS; i++) {
+            for (int j = 0; j < COLUNAS; j++) {
+                printf("%d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+
+        return 0;
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -36,5 +102,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
+     
